024d856e6ad1da419b10ea1112d6f17f46313ee2
AsmaaMohamed <smasimo81@gmail.com>
2018-03-18 10:49:58 +0200

    s

--- app/src/main/java/com/Alatheer/marmy/Fragments/Fragment_Search.java
+++ app/src/main/java/com/Alatheer/marmy/Fragments/Fragment_Search.java
@@ -3,9 +3,11 @@ package com.Alatheer.marmy.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.Alatheer.marmy.R;
 
@@ -14,12 +16,22 @@ import com.Alatheer.marmy.R;
  */
 
 public class Fragment_Search extends Fragment {
-
+EditText search;
+RecyclerView recyclerView;
+    View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search,container,false);
+         view = inflater.inflate(R.layout.fragment_search,container,false);
+
+
+        initView();
 
         return view;
+    }
+
+    private void initView() {
+        search=view.findViewById(R.id.search);
+        recyclerView=view.findViewById(R.id.RecycleSearch);
     }
 }
